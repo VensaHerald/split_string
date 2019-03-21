2d34602bc67123938ea13899688e5e7540f6d712
Sam <19407947+VensaHerald@users.noreply.github.com>
2019-03-21 16:02:43 +0000

    added print func, sorted source increment issue

--- split_string.c
+++ split_string.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <math.h>
 
+int print_ar_m(char** mult_ar, int dims);
+
 int main(void)
 {
 	//dummy input
@@ -15,27 +17,56 @@ int main(void)
 	// Calculate dimensions of square of chars
 	int dims = sqrt(length - spaces);
 	// Allocate memory for square of chars
+	// this must be array of arrays or pointer to pointers
 	char** split_str = malloc(sizeof(char)*(dims^2));
 	//set up variables for looping	
-	int x,y = 0;
+	int x = 0;
+	int y = 0;
 	char* start = in_str;
+	printf("dim : %i\n",dims);
 	// Loop through x and y axes of square assigning string values
 	// and discarding EOL
 	while (y < dims)
 	{
 		// re-zero x val
-		x = 0;
 		while (x < dims)
 		{
+			
 			if (*in_str != 10)
 			{
 				// Corrected, split_str is now char**
 				split_str[y][x] = *in_str;
-			}
-			x++;
-			in_str++;
+				x++;
+				in_str++;
+				
+			} else {in_str++;}
+			
 		}
+		x = 0;
 		y++;
 	}
+	
+	print_ar_m(split_str,dims);	
+
+
 free(split_str);
+}
+
+
+int print_ar_m(char** mult_ar, int dims)
+{
+	int y = 0,x = 0;
+	while (y < dims)
+	{
+		while (x < dims)
+		{
+			printf("%c",mult_ar[y][x]);
+			x++;
+		}
+		printf("\n");
+		// re-zero x val
+		x = 0;
+		y++;
+	}
+	return 0;
 }
