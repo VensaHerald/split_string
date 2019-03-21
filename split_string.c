4090c418aeb074796e15c3b0c193e0ec6f35f01e
Sam <19407947+VensaHerald@users.noreply.github.com>
2019-03-21 13:33:43 +0000

    initial commit

---
+++ split_string.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+	//dummy input
+	char* in_str = "abc\ndef\nghi";
+	 
+	// Get input string length
+	double length = strlen(in_str);
+	// Calculate number of EOL chars(\n) in string
+	double spaces = floor(sqrt(length)-1);
+	// Calculate dimensions of square of chars
+	int dims = sqrt(length - spaces);
+	// Allocate memory for square of chars
+	char** split_str = malloc(sizeof(char)*(dims^2));
+	//set up variables for looping	
+	int x,y = 0;
+	char* start = in_str;
+	// Loop through x and y axes of square assigning string values
+	// and discarding EOL
+	while (y < dims)
+	{
+		// re-zero x val
+		x = 0;
+		while (x < dims)
+		{
+			if (*in_str != 10)
+			{
+				// following syntax does not work.  Due to mult dim arrays not
+				// being contiguous?  Will test
+				split_str[y][x] = *in_str;
+			}
+			x++;
+			in_str++;
+		}
+		y++;
+	}
+free(split_str);
+}
